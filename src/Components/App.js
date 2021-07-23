7610dea22dedacad789eb9486925e747c301879e
Sagi <sagi.laufer23@gmail.com>
2021-07-23 13:00:59 +0300

    added title

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from "./Header";
+import Title from "./Title";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
+      <Header />
+      <Title title="explore your next"/>
+      <Title title="movies and tv shows"/>
     </div>
   );
 }
---
+++ src/Components/Title.js
@@ -0,0 +1,9 @@
+import './Title.css';
+
+function Title({ title }) {
+    return <div className="title">
+        {title}
+    </div>;
+}
+
+export default Title;
